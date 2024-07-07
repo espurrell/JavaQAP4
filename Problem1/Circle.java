ae93d8b1e9792a06c3519af739e6a115a8b8dfc8
espurrell <150044207+espurrell@users.noreply.github.com>
2024-07-07 21:15:37 -0230

    Upload working file for Problem1 - debugg complete, demo screenshot attached

---
+++ Problem1/Circle.java
@@ -0,0 +1,7 @@
+package Problem1;
+
+public class Circle extends Ellipse {
+    public Circle(double radius) {
+        super(radius, radius);
+    }
+}
---
+++ Problem1/EquilateralTriangle.java
@@ -0,0 +1,8 @@
+package Problem1;
+
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(double side) {
+        super(side, side, side);
+        super.name = "Equilateral Triangle";
+    }
+}
